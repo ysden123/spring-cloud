35d61abbc0c419cb8b0e8306b7bfe66a297db36c
Yuriy Stul <ysden123@gmail.com>
2019-06-21 10:14:29 +0300

    Config in classpath

--- config-server/src/main/java/com/stulsoft/spring/cloud/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/stulsoft/spring/cloud/config/ConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
