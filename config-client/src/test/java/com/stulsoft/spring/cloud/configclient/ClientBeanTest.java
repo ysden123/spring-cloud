0d6a7dcea9baf08f18b0f3229a4c294dda967568
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2019-06-23 14:13:09 +0300

    Unit tests

--- config-client/src/test/java/com/stulsoft/spring/cloud/configclient/ClientBeanTest.java
+++ config-client/src/test/java/com/stulsoft/spring/cloud/configclient/ClientBeanTest.java
@@ -7,7 +7,7 @@ package com.stulsoft.spring.cloud.configclient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,8 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author Yuriy Stul
  */
 @RunWith(SpringRunner.class)
-@TestPropertySource(properties = {"spring.profiles.active=test22"})
-@SpringBootTest
+@ContextConfiguration(classes = ClientBean.class)
+@TestPropertySource(locations = "classpath:app-config-test2.properties")
 public class ClientBeanTest {
     @Autowired
     ClientBean clientBean;
